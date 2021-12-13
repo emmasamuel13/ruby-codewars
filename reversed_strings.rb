b5c4e2b305624c5dcc55cee8d5256f0759cd074c
Emma Samuel <emmameaden95@gmail.com>
2021-12-13 23:45:27 +0000

    Create reversed_strings.rb

---
+++ reversed_strings.rb
@@ -0,0 +1,10 @@
+#Complete the solution so that it reverses the string passed into it.
+
+def solution(str)
+    puts str.reverse!
+end
+
+solution('world')
+solution('hello')
+solution('')
+solution('h')
