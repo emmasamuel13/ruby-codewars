79c33f49c5d38f510d727a6efea0db5f766edd30
Emma Samuel <emmameaden95@gmail.com>
2021-12-14 00:13:21 +0000

    Create vowel_count.rb

---
+++ vowel_count.rb
@@ -0,0 +1,17 @@
+#Return the number (count) of vowels in the given string. We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+def get_count(input_str)
+    letters = input_str.downcase.split("")
+    vowels = ["a", "e", "i", "o", "u"]
+    count = 0
+    letters.each do |ind_lett|
+        count += 1 if vowels.include?(ind_lett)
+    end
+    puts count
+end
+
+get_count("abracadabra")
+get_count("")
+get_count("pear tree")
+get_count("o a kak ushakov lil vo kashu kakao")
+get_count("tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty")
