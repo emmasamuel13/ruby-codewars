eec893ffb1c3ee8c5e2dedada2a4dcb9130b2806
Emma Samuel <emmameaden95@gmail.com>
2021-12-20 23:31:11 +0000

    Create shortest_word.rb

---
+++ shortest_word.rb
@@ -0,0 +1,19 @@
+#Simple, given a string of words, return the length of the shortest word(s). String will never be empty and you do not need to account for different data types.
+
+def find_short(s)
+    arr = s.downcase.split(" ")
+    lengths = ()
+    arr.each do |word|
+        lengths << word.length
+    end
+    lengths.sort!
+    l = [lengths[0]].to_i
+    puts l
+end
+
+find_short("bitcoin take over the world maybe who knows perhaps")
+find_short("turns out random test cases are easier than writing out basic ones")
+find_short("lets talk about javascript the best language")
+find_short("i want to travel the world writing code one day")
+find_short("Lets all go on holiday somewhere very cold")
+find_short("Let's travel abroad shall we")
