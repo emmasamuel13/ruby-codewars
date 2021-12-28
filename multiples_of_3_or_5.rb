9f602c71e6109e91f40cb9db008e80e794f8b27e
Emma Samuel <emmameaden95@gmail.com>
2021-12-28 21:32:16 +0000

    Create multiples_of_3_or_5.rb

---
+++ multiples_of_3_or_5.rb
@@ -0,0 +1,21 @@
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them). Note: If the number is a multiple of both 3 and 5, only count it once.
+
+def solution(number)
+    all = []
+    if number > 0
+        all = (0..number-1).to_a
+    end
+    divisible = []
+    all.each do |int|
+        if int % 3 == 0 || int % 5 == 0
+            divisible << int
+        end
+    end
+    sum = 0
+    divisible.each{ |n| sum+=n }
+    puts sum
+end
+
+solution(10)
+solution(20)
+solution(200)
